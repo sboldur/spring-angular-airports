5f3194247afb0505147654b76f23e63e1c4058ef
sboldur <simona.diana.boldur@gmail.com>
2017-09-28 14:53:46 +0300

    initial commit

---
+++ src/main/java/com/sboldur/springangularairports/SpringAngularAirportsApplication.java
@@ -0,0 +1,12 @@
+package com.sboldur.springangularairports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularAirportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularAirportsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sboldur/springangularairports/SpringAngularAirportsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sboldur.springangularairports;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAngularAirportsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
